4a7d776376fc94b30ba31e5929a23832d2900072
Andrey Prikhodko <5ray69@ukr.net>
2024-04-09 16:07:40 +0300

    Добавьте файлы проекта.

---
+++ Model/MathFuncs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMhabr.Model
+{
+    internal class MathFuncs
+    {
+        public int GetSumOf(int a, int b) => a + b;
+    }
+}
